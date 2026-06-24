86c329f86fdc837080dbf603d969cf6004f5b3cc
STEPSIND <santiago.g.steps@gmail.com>
2026-06-24 15:26:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ import Catalogo from './pages/Catalogo'
 import CatalogoPublico from './pages/CatalogoPublico'
 import CargaProductos from './pages/CargaProductos'
 import Remitos from './pages/Remitos'
-import ListaPrecios from './ListaPrecios'
+import ListaPrecios from './pages/ListaPrecios'
 
 // Gestión
 import SectorGestion from './pages/GESTION'
